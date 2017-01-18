32bd8d2d1dec020c7ffa79101d51090ac639730a
tamayo88 <alberto_tamayo_bazuelo@hotmail.com>
2017-01-18 10:39:04 +0100

    Update InvertirDigito.java
    Añadidos algunos comentarios

--- InvertirDigito.java
+++ InvertirDigito.java
@@ -1,29 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package invertirdigito;
 
 import java.util.Scanner;
 
-/**
- *
- * @author usuario
- */
 public class InvertirDigito {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     //variable num guardara valor que se pedira por teclado
        int num;
+        // Bucle do while para controlar que el usuario introduzca un numero de 5 digitos
         do{
         System.out.println("Introduce el numero de 5 digitos");
                   Scanner sc2 = new Scanner(System.in);
                    num = sc2.nextInt();
         }while(num<9999 && num>99999);
+        
         String str = String.valueOf(num) ;
 String rpta = "" ;
 
